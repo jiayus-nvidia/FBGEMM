31f4c11bae954de7d9cd18cc7ca81ef2b2843409
junzhang <junzhang@nvidia.com>
2026-07-08 15:07:13 -0700

    Enable fused output writeback

--- fbgemm_gpu/experimental/hstu/src/hstu_blackwell/hstu_fwd.py
+++ fbgemm_gpu/experimental/hstu/src/hstu_blackwell/hstu_fwd.py
@@ -1451,8 +1451,6 @@ class HSTUAttentionForwardSm100:
         )
         cute.copy(thr_tmem_load, tStS_t2r, output_values)
         cute.arch.fence_view_async_tmem_load()
-        if const_expr(self.debug):
-            return
         inv_seqlen = Float32(1.0 / 128.0)
         for i in cutlass.range_constexpr(cute.size(output_values)):
             coord = tScO_t2r[i]
